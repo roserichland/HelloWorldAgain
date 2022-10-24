c0dda97619e136639416ac1616610791a3891546
rosemiller <rose.miller@pcc.edu>
2022-10-24 14:10:36 -0700

    added another print statement specifying that this was the date being read

--- src/edu/pcc/cis/HelloWorldAgain/HelloWorldAgainClass.java
+++ src/edu/pcc/cis/HelloWorldAgain/HelloWorldAgainClass.java
@@ -7,5 +7,6 @@ public class HelloWorldAgainClass {
 public static void main(String[] args) {
       System.out.println("Hello world. Can you hear me now?");
       System.out.println(new Date());
+      System.out.println("This is the time and date, world.");
     }
 }
