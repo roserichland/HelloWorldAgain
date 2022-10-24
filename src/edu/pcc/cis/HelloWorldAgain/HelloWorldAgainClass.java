5764150b38dcf0f6d07238bee42a9e323eef79d3
rosemiller <rose.miller@pcc.edu>
2022-10-24 13:53:37 -0700

    added new feature of reading the date as well

--- src/edu/pcc/cis/HelloWorldAgain/HelloWorldAgainClass.java
+++ src/edu/pcc/cis/HelloWorldAgain/HelloWorldAgainClass.java
@@ -1,9 +1,11 @@
 package edu.pcc.cis.HelloWorldAgain;
+import java.util.Date;
 /**
  * Created my Rose Miller on 10/24/2022
  */
 public class HelloWorldAgainClass {
 public static void main(String[] args) {
       System.out.println("Hello world. Can you hear me now?");
+      System.out.println(new Date());
     }
 }
