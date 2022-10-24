1c85e8a4fdf0b7da4a2a3a9f358c7d2d8e7be468
rosemiller <rose.miller@pcc.edu>
2022-10-24 13:50:13 -0700

    Created HelloWorldAgainClass class and fixed up .gitignore

---
+++ src/edu/pcc/cis/HelloWorldAgain/HelloWorldAgainClass.java
@@ -0,0 +1,9 @@
+package edu.pcc.cis.HelloWorldAgain;
+/**
+ * Created my Rose Miller on 10/24/2022
+ */
+public class HelloWorldAgainClass {
+public static void main(String[] args) {
+      System.out.println("Hello world. Can you hear me now?");
+    }
+}
